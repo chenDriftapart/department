9e073ebeb0e7340ac0b6f25bbd45237869e1d61a
chen_xs <1814818318@qq.com>
2021-05-13 16:39:30 +0800

    代码更新

--- src/main/java/com/example/department/controller/indexController.java
+++ src/main/java/com/example/department/controller/indexController.java
@@ -3,6 +3,8 @@ package com.example.department.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class indexController {
 
@@ -15,4 +17,14 @@ public class indexController {
     public String newGroup() {
         return "new_group";
     }
+
+    @RequestMapping("/login")
+    public String login(HttpSession session) {
+        Object user = session.getAttribute("user");
+        if (user != null) {
+            return "bootstrap";
+        }
+        return "redirect:/";
+    }
+
 }
